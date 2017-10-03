f5316983e9520f95ff19c696b5689d8833933013
keithpurr <keithpullout@gmail.com>
2017-10-02 19:08:21 -0600

    Stopped all motion when hit wall thru code ( may not be right).

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -4,36 +4,57 @@ using System.Collections;
 public class Controller : MonoBehaviour
 {
     public float speed = 2.0f;
+    public float powerMagnitude = 10.0f;
+
 	private float startTime;
 	private Vector3 startPosition;
 	private Vector3 targetPosition;
     private float journeyLength = 1.0f;
 
-
-
+    //private Rigidbody _rigidbody;
+    //private Vector3 force;
+    //private bool buttonPushed = false;
 
     void Start(){
         
         startPosition = transform.position;
         targetPosition = startPosition;
         startTime = Time.time;
+
+        //_rigidbody = GetComponent<Rigidbody>();
+
     }
 
 
 	void Update(){
 
-			float distCovered = (Time.time - startTime) * speed;
-			float fracJourney = distCovered / journeyLength;
-            transform.position = Vector3.Lerp(startPosition, targetPosition, fracJourney);
+        float distCovered = (Time.time - startTime) * speed;
+        float fracJourney = distCovered / journeyLength;
+        transform.position = Vector3.Lerp(startPosition, targetPosition, fracJourney);
+        //if (buttonPushed)
+        //{
+        //    _rigidbody.AddForce(force * powerMagnitude);
+        //    buttonPushed = false;
+        //}
 
 		}
 
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.tag == "Wall"){
+            Debug.Log("hitwall");
+            targetPosition = transform.position;
+        }
+    }
+
     public void MoveUpWard(){
 
-		startTime = Time.time;
+        startTime = Time.time;
         startPosition = transform.position;
         targetPosition = startPosition + Vector3.forward;
 
+        //force = Vector3.forward;
+        //buttonPushed = true;
         }
 
 	public void MoveDownWard()
